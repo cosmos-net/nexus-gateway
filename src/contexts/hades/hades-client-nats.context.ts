880eb002c8d01d289433310eb720790935d672ae
oztin <roberto.austin@innovationsmi.com>
2024-12-11 21:46:53 -0800

    chore: Add Hades client NATS context module for microservices communication

---
+++ src/contexts/hades/hades-client-nats.context.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+import { HADES_CLIENT_CONTEXT_PROXY_NAME } from '@hades/commons/commands-name';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ClientsModule.register([
+      {
+        name: HADES_CLIENT_CONTEXT_PROXY_NAME,
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://localhost:4222'],
+        },
+      },
+    ]),
+  ],
+  exports: [
+    ClientsModule.register([
+      {
+        name: HADES_CLIENT_CONTEXT_PROXY_NAME,
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://localhost:4222'],
+        },
+      },
+    ]),
+  ],
+})
+export class MusesClientNatsModule {}
