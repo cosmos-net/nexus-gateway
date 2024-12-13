cc14fe3f8c8ff646ee23f463c7230b152e37f264
2314-RAustin <0315108515@miutt.edu.mx>
2024-12-13 11:09:16 -0800

    chore: Correct spelling of 'ROL' to 'ROLE' in commands and endpoints

--- src/contexts/hades/commons/end-points.ts
+++ src/contexts/hades/commons/end-points.ts
@@ -1,4 +1,4 @@
 const ENDPOINT_BASE = 'hades-context/v1';
-export const HADES_ROL_ENDPOINT = `${ENDPOINT_BASE}/rol-management`;
+export const HADES_ROLE_ENDPOINT = `${ENDPOINT_BASE}/rol-management`;
 export const HADES_USER_ENDPOINT = `${ENDPOINT_BASE}/user-management`;
 export const SESSION_USER_ENDPOINT = `${ENDPOINT_BASE}/session-management`;
