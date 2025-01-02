06f0241cf2cc859b498d97ac940dfb242f353306
Jesús Crespo <crespojesus9@gmail.com>
2025-01-02 14:28:08 -0800

    feat: :sparkles: user session controller settings to interact with function events.

--- src/contexts/hades/commons/end-points.ts
+++ src/contexts/hades/commons/end-points.ts
@@ -1,4 +1,4 @@
 const ENDPOINT_BASE = 'hades-context/v1';
 export const HADES_ROLE_ENDPOINT = `${ENDPOINT_BASE}/role-management`;
 export const HADES_USER_ENDPOINT = `${ENDPOINT_BASE}/user-management`;
-export const SESSION_USER_ENDPOINT = `${ENDPOINT_BASE}/session-management`;
+export const HADES_SESSION_ENDPOINT = `${ENDPOINT_BASE}/session-management`;
