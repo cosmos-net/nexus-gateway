019e9eb499b6fa3a8634118fd62791aab2c02a49
Jesús Crespo <crespojesus9@gmail.com>
2025-01-02 14:28:08 -0800

    feat: :sparkles: user session controller settings to interact with function events.

---
+++ src/contexts/hades/sessions/update-session.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Inject, Patch } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+import { recoverErrorType } from '@common/helpers/error-management';
+import { HADES_CLIENT_CONTEXT_PROXY_NAME, COMMANDS_HADES } from '@hades/commons/commands-name';
+import { HADES_SESSION_ENDPOINT } from '@hades/commons/end-points';
+
+@Controller(HADES_SESSION_ENDPOINT)
+export class UpdateSessionController {
+  constructor(
+    @Inject(HADES_CLIENT_CONTEXT_PROXY_NAME)
+    private readonly clientProxy: ClientProxy,
+  ) {}
+
+  @Patch()
+  async update(@Body() input: unknown): Promise<unknown> {
+    try {
+      const output = await lastValueFrom(
+        this.clientProxy.send({ cmd: COMMANDS_HADES.SESSION.UPDATE }, input),
+      );
+
+      return output;
+    } catch (error) {
+      throw recoverErrorType(error);
+    }
+  }
+}
