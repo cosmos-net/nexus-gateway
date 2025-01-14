32fe2677cfd65fadb4f82e42ea9e0110a352926d
oztin <0315108515@miutt.edu.mx>
2025-01-13 22:32:03 -0800

    feat: :sparkles: add CreateActiveSessionController for managing active session creation

---
+++ src/contexts/hades/sessions/create-active-session.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+
+import { recoverErrorType } from '@common/helpers/error-management';
+import { COMMANDS_HADES, HADES_CLIENT_CONTEXT_PROXY_NAME } from '@hades/commons/commands-name';
+import { HADES_SESSION_ENDPOINT } from '@hades/commons/end-points';
+
+@Controller(HADES_SESSION_ENDPOINT)
+export class CreateActiveSessionController {
+  constructor(
+    @Inject(HADES_CLIENT_CONTEXT_PROXY_NAME)
+    private readonly clientProxy: ClientProxy,
+  ) {}
+
+  @Post('active')
+  async create(@Body() input: unknown): Promise<unknown> {
+    try {
+      const output = await lastValueFrom(
+        this.clientProxy.send({ cmd: COMMANDS_HADES.SESSION.CREATE_ACTIVE }, input),
+      );
+
+      return output;
+    } catch (error) {
+      throw recoverErrorType(error);
+    }
+  }
+}
